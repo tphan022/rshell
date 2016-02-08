b155343abbb5fc00dc9c54b80f015ed11c850ac5
Tony Phan <tphan022@ucr.edu>
2016-02-07 21:52:54 -0800

    finished creating the user input function

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 
-int main() {
+// This display function displays the prompt and recieves the user input
+void display(char& argument) {
+	while(1) {
+		char* userinfo = getlogin();
+		char location[20];
+		gethostname(location,20);
+		cout << "<" << userinfo << "@" << location << ">" << "$";
+		fgets(&argument,1024,stdin);
+		if(argument != '\n') {
+			break;
+		}
+	}
+}
 
 
+
+
+int main(void) {
+	char argument[1024];
+	char* command[64];
+	while(1) {
+		display(*argument);
+	}
 
 return 0;
 }
-
