71e75a9049c9153332c259ffde6df5de59e4424c
Tony Phan <tphan022@hammer.cs.ucr.edu>
2016-02-12 03:59:06 -0800

    added test files

---
+++ tests/commented_command.sh
@@ -0,0 +1,20 @@
+Script started on Fri 12 Feb 2016 03:49:04 AM PST
+]0;tphan022@hammer:~/rshell/tests[?1034h[tphan022@hammer tests]$ exit/home/csmajs/tphan022/rshell/bin/rshell
+<tphan022@hammer.cs.ucr.edu>$ ls # echo hello
+commented_command.sh  exit.sh  multi_command.sh  single_command.sh
+<tphan022@hammer.cs.ucr.edu>$ pwd # ls && mkdir newdir; echo hello
+/home/csmajs/tphan022/rshell/tests
+<tphan022@hammer.cs.ucr.edu>$ ls -a# echo hello
+ls: invalid option -- '#'
+Try `ls --help' for more information.
+<tphan022@hammer.cs.ucr.edu>$ ls -a #echo hello
+ls: cannot access #echo: No such file or directory
+ls: cannot access hello: No such file or directory
+<tphan022@hammer.cs.ucr.edu>$ pwd # this is a comment
+/home/csmajs/tphan022/rshell/tests
+<tphan022@hammer.cs.ucr.edu>$ exit
+Program Exited.
+]0;tphan022@hammer:~/rshell/tests[tphan022@hammer tests]$ exit
+exit
+
+Script done on Fri 12 Feb 2016 03:51:37 AM PST
--- tests/exit.sh
+++ tests/exit.sh
@@ -1,2 +1,26 @@
-#!/bin/sh
-#This bash script handles the exit command and the command attached to actions
+Script started on Fri 12 Feb 2016 03:53:39 AM PST
+]0;tphan022@hammer:~/rshell/tests[?1034h[tphan022@hammer tests]$ exit/home/csmajs/tphan022/rshell/bin/rshell
+<tphan022@hammer.cs.ucr.edu>$ exit
+Program Exited.
+]0;tphan022@hammer:~/rshell/tests[tphan022@hammer tests]$ /home/csmajs/tphan022/rshell/bin/rshell[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cexit[K/home/csmajs/tphan022/rshell/bin/rshell
+<tphan022@hammer.cs.ucr.edu>$ ls; exit
+commented_command.sh  exit.sh  multi_command.sh  single_command.sh
+Program Exited.
+]0;tphan022@hammer:~/rshell/tests[tphan022@hammer tests]$ /home/csmajs/tphan022/rshell/bin/rshell
+<tphan022@hammer.cs.ucr.edu>$ ls -a && pwd; mkdir testingdir && ls; rm -rf testingdir, ; ls && exit
+.  ..  commented_command.sh  exit.sh  multi_command.sh	single_command.sh
+/home/csmajs/tphan022/rshell/tests
+commented_command.sh  exit.sh  multi_command.sh  single_command.sh  testingdir
+commented_command.sh  exit.sh  multi_command.sh  single_command.sh
+Program Exited.
+]0;tphan022@hammer:~/rshell/tests[tphan022@hammer tests]$ /home/csmajs/tphan022/rshell/bin/rshell
+<tphan022@hammer.cs.ucr.edu>$ ped  wd || exit
+/home/csmajs/tphan022/rshell/tests
+<tphan022@hammer.cs.ucr.edu>$ pwd # exit
+/home/csmajs/tphan022/rshell/tests
+<tphan022@hammer.cs.ucr.edu>$ exit
+Program Exited.
+]0;tphan022@hammer:~/rshell/tests[tphan022@hammer tests]$ exit
+exit
+
+Script done on Fri 12 Feb 2016 03:56:49 AM PST
